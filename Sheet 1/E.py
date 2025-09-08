edd3d118da669ed9bab884fd42b099a6ea895404
durjoy1971 <durjoybarua1971@gmail.com>
2025-09-08 22:28:37 +0600

    Add E.py for area calculation of a circle

---
+++ Sheet 1/E.py
@@ -0,0 +1,5 @@
+a = float(input())
+
+pie = 3.141592653
+
+print(f"{pie * (a ** 2):.9f}")
--- main.py
+++
@@ -1,8 +0,0 @@
-tokens = input().split()
-
-a = int(tokens[0])
-b = int(tokens[1])        
-c = int(tokens[2])
-d = int(tokens[3])
-
-print("Difference = {}".format((a * b) - (c * d)))
